2617bfddf4dd3c3a4a4efcbe30abb325990088f8
xiewendan <stephenxjc@gmail.com>
2019-06-26 22:08:21 +0800

    #0 release script

--- tools/dockerhubpush.sh
+++ tools/dockerhubpush.sh
@@ -2,5 +2,5 @@
 docker push stephenxjc/taiga-docker-events:v2
 docker push stephenxjc/taiga-docker-front:v2
 docker push stephenxjc/taiga-docker-back:v2
-docker push taiga-front-dist-gen:v3
-docker push taiga-front-dist-gen-release:v3
+docker push stephenxjc/taiga-front-dist-gen:v3
+docker push stephenxjc/taiga-front-dist-gen-release:v3
